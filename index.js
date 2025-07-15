459f19f99cb47ca88a94ec133c22ecb21150b170
Chitranjan kumar <chitranjan75075@gmail.com>
2025-07-15 20:35:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function change(){
+    let ele=document.getElementsByTagName("h1")
+    ele[0].innerText="hii everyone"
+    ele[0].style.backgroundColor="red"
+    let ele1=document.getElementById("param")
+    ele1.innerHTML="<h1> heyy...."
+   let ele2 = document.getElementById("main");
+    // ele2.style.display ="flex"
+    ele2.style.flexDirection="column"
+
+
+}
